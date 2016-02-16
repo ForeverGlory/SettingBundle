6bc7ec0325cb8bab3eee9b41238393d2b5dd507c
ForeverGlory <foreverglory@qq.com>
2016-02-16 20:03:07 +0800

    model

---
+++ Document/Setting.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Glory\SettingBundle\Document;
+
+use Glory\SettingBundle\Model\Setting as AbstractSetting;
+
+/**
+ * Setting
+ */
+class Setting extends AbstractSetting
+{
+    
+}
---
+++ Entity/Setting.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Glory\SettingBundle\Entity;
+
+use Glory\SettingBundle\Model\Setting as AbstractSetting;
+
+/**
+ * Setting
+ */
+class Setting extends AbstractSetting
+{
+    
+}
