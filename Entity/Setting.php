5e7c9fb89d8de3093e0b300be53f07726e538f6f
ForeverGlory <foreverglory@qq.com>
2016-03-30 16:06:31 +0800

    use entity

--- Entity/Setting.php
+++ Entity/Setting.php
@@ -3,11 +3,39 @@
 namespace Glory\Bundle\SettingBundle\Entity;
 
 use Glory\Bundle\SettingBundle\Model\Setting as AbstractSetting;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Setting
+ *
+ * @ORM\Table(name="setting")
+ * @ORM\Entity
  */
 class Setting extends AbstractSetting
 {
-    
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    protected $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="text")
+     */
+    protected $value;
+
+
 }
+
